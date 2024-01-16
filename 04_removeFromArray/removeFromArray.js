18a99849da8e392dd750d145d370f2b0deebf58c
Cristoval Wenoch <wenochcristoval@gmail.com>
2024-01-16 18:34:57 +0700

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+  let newArray = [];
 
+  for (let element of array) {
+    let include = true;
+
+    for (let value of values) {
+      if (element === value) include = false;
+    }
+
+    if (include) newArray.push(element);
+  }
+
+  return newArray;
 };
 
 // Do not edit below this line
