bd4a4c84bc677d78a72a44c18871c91373d4cc77
Cristoval Wenoch <wenochcristoval@gmail.com>
2024-01-25 14:20:06 +0700

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  str = str.replaceAll(/[^a-z0-9]/gi, "");
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
 
+  return str.toLowerCase() === reversed.toLowerCase();
 };
 
 // Do not edit below this line
