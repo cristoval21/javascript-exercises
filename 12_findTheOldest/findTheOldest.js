68d4bee1905be045ae2cf3efbf0e770c9942d553
Cristoval Wenoch <wenochcristoval@gmail.com>
2024-01-25 19:41:27 +0700

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people = []) {
+  let oldestAge = 0;
+  let currentYear = (new Date()).getFullYear();
+  return people.reduce((acc, person) => {
+    const age = (person.yearOfDeath ? person.yearOfDeath : currentYear) - person.yearOfBirth;
+    if (age > oldestAge) {
+      oldestAge = age;
+      acc = person;
+    }
 
+    return acc
+  }, {});
 };
 
 // Do not edit below this line
