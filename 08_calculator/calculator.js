66acbbc83bce391538197ce512556d39799766c6
Cristoval Wenoch <wenochcristoval@gmail.com>
2024-01-25 07:42:48 +0700

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return +(nums.reduce((total, num) => total + num, 0));
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return +(nums.reduce((total, num) => total * num));
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let total = 1;
+	for (let i = 0; i < b; i++) {
+    total *= a;
+  }
+
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+	if (num == 0) {
+    return 1;
+  } else {
+    for (let i = 1; i <= num; i++) {
+      total *= i;
+    }
+  }
+
+  return total;
 };
 
 // Do not edit below this line
